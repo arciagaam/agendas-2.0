dfb101f49df5e3ade598386e7a03100761dfeca9
allenJcodes <110220645+allenJcodes@users.noreply.github.com>
2023-07-19 03:19:19 +0800

    schedule template create style changes

--- resources/views/pages/admin/information/schedule_templates/create.blade.php
+++ resources/views/pages/admin/information/schedule_templates/create.blade.php
@@ -1,6 +1,6 @@
 <x-main-layout>
 
-    <div class="flex gap-5">
+    <div class="flex gap-5 border p-4 rounded-lg">
         <div class="form-input-container">
             <label class="text-sm" for="grade_level_id">Grade Level</label>
             
@@ -41,35 +41,35 @@
         </div>
     </div>
 
-    <div id="table_container" class="flex flex-wrap gap-5">
-        <table data-tableNumber="1" class="mt-12 table-auto border-separate border-spacing-2">
+    <div id="table_container" class="flex flex-wrap gap-5 border p-4 rounded-lg">
+        <table data-tableNumber="1" class="table-auto border-separate border-spacing-2">
             <thead>
                 <tr class="relative text-center">
                     <th>Time</th>
     
                     <th aria-colindex="1" class="">
-                        <div class="flex flex-col gap-5 w-full">
+                        <div class="flex flex-col w-full p-5">
                             <x-timetable-selection/>
                             <p class="align-middle w-full">Monday</p>
                         </div>
                     </th>
     
                     <th aria-colindex="2" class="">
-                        <div class="flex flex-col gap-4">
+                        <div class="flex flex-col w-full p-5">
                             <x-timetable-selection/>
                             <p>Tuesday</p>
                         </div>
                     </th>
     
                     <th aria-colindex="3" class="">
-                        <div class="flex flex-col gap-4">
+                        <div class="flex flex-col w-full p-5">
                             <x-timetable-selection/>
                             <p>Wednesday</p>
                         </div>
                     </th>
     
                     <th aria-colindex="4" class="">
-                        <div class="flex flex-col gap-4">
+                        <div class="flex flex-col w-full p-5">
                             <x-timetable-selection/>
                             <p>Thursday</p>
                         </div>
@@ -77,7 +77,7 @@
                     
     
                     <th aria-colindex="5" class="">
-                        <div class="flex flex-col gap-4">
+                        <div class="flex flex-col w-full p-5">
                             <x-timetable-selection/>
                             <p>Friday</p>
                         </div>
@@ -88,8 +88,8 @@
             </thead>
             <tbody>
                 <tr class="text-center">
-                    <td>
-                        <div class="flex flex-col">
+                    <td class="td-container gap-3 w-full p-5">
+                        <div class="flex flex-col p-5">
                             <input class="absolute top-0 left-0 h-1/2 w-full bg-transparent outline-0 cursor-pointer text-center" type="time" name="time_start[]">
                             <input class="absolute bottom-0 left-0 h-1/2 w-full bg-transparent outline-0 cursor-pointer text-center" type="time" name="time_end[]">
                         </div>
@@ -127,8 +127,8 @@
     
                     <td class="body-actions">
                         <div class="flex gap-2">
-                            <button type="button" class="remove-row bg-red-500 p-1 px-2">-</button>
-                            <button type="button" class="add-row bg-green-500 p-1 px-2">+</button>
+                            <button type="button" class="remove-row bg-red-500 py-2 px-4 rounded-lg">-</button>
+                            <button type="button" class="add-row bg-green-500 py-2 px-4 rounded-lg">+</button>
                         </div>
                     </td>
                 </tr>
@@ -137,7 +137,7 @@
         </table>
     </div>
 
-    <button id="submit_schedule_template">SAVE</button>
+    <button class="w-fit self-end rounded-lg px-4 py-2 bg-project-dominant text-project-primary ring-1 ring-project-gray-default hover:text-project-accent hover:ring-project-accent duration-100" id="submit_schedule_template">Save</button>
 
 
 </x-main-layout>
